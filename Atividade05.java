961322c688ffd55a55721dd3d3a2a2030af756d0
domcaue <144695640+domcaue@users.noreply.github.com>
2023-11-13 12:22:38 -0300

    Add files via upload

---
+++ Atividade05.java
@@ -0,0 +1,8 @@
+package Atividades;
+
+public class Atividade05 {
+    public static void main(String[] args) {
+        int nasc = 2005, ano = 2023;
+        System.out.println(ano-nasc);
+    }
+}
